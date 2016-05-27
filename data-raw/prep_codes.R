90be4937753e1ee8f20060f7009052893c2b1927
Kyle Walker <kew331@gmail.com>
2016-05-26 22:10:50 -0500

    add new county names for 2015 in SD and AK

--- data-raw/prep_codes.R
+++ data-raw/prep_codes.R
@@ -12,5 +12,12 @@ fips_codes <- left_join(county_codes, state_codes, by = "state_code")
 
 fips_codes <- select(fips_codes, state = state.x, state_code, state_name, county_code, county)
 
+new_counties <- data.frame(state = c('SD', 'AK'), state_code = c('46', '02'),
+                           state_name = c('South Dakota', 'Alaska'), county_code = c('102', '158'),
+                           county = c('Oglala Lakota County', 'Kusilvak Census Area'))
+
+fips_codes <- rbind(fips_codes, new_counties) %>%
+  arrange(state_code, county_code)
+
 
 save(fips_codes, file = "data/fips_codes.rda")
