d3d2cc41039003106c8e891e17a24bd402d3f469
Kyle Walker <kyle@walker-data.com>
2025-05-17 10:11:38 -0500

    fix #203

--- R/metro_areas.R
+++ R/metro_areas.R
@@ -142,11 +142,19 @@ urban_areas <- function(cb = FALSE, year = NULL, criteria = NULL, ...) {
         if (year == 2013) url <- gsub("shp/", "", url)
     } else {
         if (year >= 2023) {
-            url <- sprintf(
-                "https://www2.census.gov/geo/tiger/TIGER%s/UAC/tl_%s_us_uac20.zip",
-                cyear,
-                cyear
-            )
+            if (year == 2023) {
+                url <- sprintf(
+                    "https://www2.census.gov/geo/tiger/TIGER%s/UAC/tl_%s_us_uac20.zip",
+                    cyear,
+                    cyear
+                )
+            } else {
+                url <- sprintf(
+                    "https://www2.census.gov/geo/tiger/TIGER%s/UAC20/tl_%s_us_uac20.zip",
+                    cyear,
+                    cyear
+                )
+            }
         } else if (!is.null(criteria) && criteria == "2020") {
             if (year != 2020) {
                 stop(
